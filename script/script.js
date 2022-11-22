66b6aefe92414347a5722ac2077b47f245542cc2
Maia Bell <maiarose04@gmail.com>
2022-11-22 10:39:33 -0800

    Adds another event listener

--- script/script.js
+++ script/script.js
@@ -9,18 +9,38 @@ alert("Button Clicked")
 // Making ways to only alert the event listener once
 
 // Option 1:
-clickMeButton.addEventListener("click", function clickButton()
-{
-    alert("You Clicked");
-    clickMeButton.removeEventListener("click", clickButton);
-});
+// clickMeButton.addEventListener("click", function clickButton()
+// {
+//     alert("You Clicked");
+//     clickMeButton.removeEventListener("click", clickButton);
+// });
 
 // Option 2:
-clickMeButton.addEventListener("click", clickButton, {once : true})
+// clickMeButton.addEventListener("click", clickButton, {once : true})
 
 // Option 3:
-clickMeButton.addEventListener("click", function clickButton()
-{
-    alert("You Clicked");
-}, {once : true});
+
+// clickMeButton.addEventListener("click", function clickButton()
+// {
+//     alert("You Clicked");
+// }, {once : true});
+
+
+// Makes background color pink
+
+// Option 1
+// clickMeButton.addEventListener("click", function clickButton(){
+// document.body.style.backgroundColor = 'pink';
+// });
+
+// Option 2
+// clickMeButton.addEventListener("click", function clickButton(){
+// document.body.classList.add("pink");
+// });
+
+clickMeButton.addEventListener("click", function changeTest(){
+    if (clickMeButton.textContent === "Click Me")
+    {clickMeButton.textContent = "Click me again?"}
+    
+})
 
