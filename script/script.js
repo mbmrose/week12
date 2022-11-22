2914d0cd945000120efb651844aa809389116eb4
Maia Bell <maiarose04@gmail.com>
2022-11-22 10:22:59 -0800

    Removes event listener

--- script/script.js
+++ script/script.js
@@ -6,5 +6,21 @@ console.log(clickMeButton);
 alert("Button Clicked")
 }
 
+// Making ways to only alert the event listener once
 
-clickMeButton.addEventListener("click", clickButton)
+// Option 1:
+clickMeButton.addEventListener("click", function clickButton()
+{
+    alert("You Clicked");
+    clickMeButton.removeEventListener("click", clickButton);
+});
+
+// Option 2:
+clickMeButton.addEventListener("click", clickButton, {once : true})
+
+// Option 3:
+clickMeButton.addEventListener("click", function clickButton()
+{
+    alert("You Clicked");
+}, {once : true});
+
